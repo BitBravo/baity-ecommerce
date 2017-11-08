ccc49f08ed1a406cab8a38cd16926e7e26323991
Muath Alkhalaf <muath.alkhalaf@gmail.com>
2017-11-08 20:05:49 +0300

    moved all code for image upload to image adder. ImageUploader becomes a wrapper for dropzone to add ability of preview

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,189 +1,111 @@
 import React from "react";
 import Dropzone from "react-dropzone";
 import PropTypes from "prop-types";
-import { app } from "../base";
-import firebase from 'firebase'
+import { Image, Grid, Row, Col, Panel } from 'react-bootstrap'
 
 class ImageUploader extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      files: []
-    };
-    //change to true if you want to upload multiple images per product
-    this.multipleFiles = false
-    // this.clickInput = this.clickInput.bind(this);
-    // this.handleFiles = this.handleFiles.bind(this);
-    // this.convertFilesToBase64 = this.convertFilesToBase64.bind(this);
+    
+    
   }
-
-  // clickInput() {
-  //   let element = document.getElementById(this.state.elementId);
-  //   element.value = '';
-  //   element.click();
-  // }
-
-  // handleFiles(event) {
-  //   if(this.props.base64) {
-  //     this.convertFilesToBase64(event.target.files);
-  //   } else {
-  //     this.props.handleFiles(event.target.files);
-  //   }
-  // }
 
   
-  componentWillUnmount() {
-    //to avoid memory leaks. See Important note @ (https://react-dropzone.js.org/)
-    this.state.files.map(file => {
-      window.URL.revokeObjectURL(file.preview);
-    })
-  }
 
-  uploadImages() {
-    //Check (https://firebase.google.com/docs/storage/web/upload-files) for more info
-    this.state.files.map(file => {
-      //get a reference for the image bucket (the placeholder where we will put the image into)
-      var imagesRef = app
-        .storage()
-        .ref()
-        .child("testProductImages/" + Date.now() + Math.random());
-      //upload the image. This is a task that will run async. Notice that it accepts a file as in
-      //browser API File (see https://developer.mozilla.org/en-US/docs/Web/API/File)
-      var metadata = {
-        contentType: file.type
-      };
-      //The following will return a task that will execte async
-      var uploadTask = imagesRef.put(file, metadata);
-      // Register three observers:
-      // 1. 'state_changed' observer, called any time the state changes
-      // 2. Error observer, called on failure
-      // 3. Completion observer, called on successful completion
-      uploadTask.on('state_changed', function(snapshot){
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-          case firebase.storage.TaskState.PAUSED: // or 'paused'
-            console.log('Upload is paused');
-            break;
-          case firebase.storage.TaskState.RUNNING: // or 'running'
-            console.log('Upload is running');
-            break;
-        }
-      }, function(error) {        
-        // Handle unsuccessful uploads
-        console.log('error uploading image of product')
-      }, function() {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-        var downloadURL = uploadTask.snapshot.downloadURL;
-        console.log('upload sucessful and image URL is: ' + downloadURL)
-      });
-    });
-  }
+  
+ 
 
-  handleOnDrop(files, rejectedFiles) {
-    var newFiles = [];
-    //always (1) copy state value, (2) change the value, (3) then assign it back to state
-    (this.multipleFiles)
-    ? newFiles = [...this.state.files, ...files]
-    : newFiles = [...files] //allow one image only and overwrite previous one
-    this.setState({
-      files: newFiles
-    });
+    
 
-    // files.map((file) => {
-    //   newFiles.push(file);
-
-    // const reader  = new FileReader();
-
-    // reader.onload = (upload) =>  {
-    //   console.log("upload opject:")
-    //   console.log(upload)
-    //   const newFilesSrcs = this.state.filesSrcs;
-    //   const fileSrc = reader.result;
-    //   newFilesSrcs.push(fileSrc);
-    //   this.setState({
-    //     filesSrcs: newFilesSrcs
-    //   })
-    // }
-
-    // if (file) {
-    //   reader.readAsDataURL(file);
-    // }
-    // });
-  }
-
-  // convertFilesToBase64(files) {
-  //   let ef = files;
-
-  //   if (this.props.multipleFiles) {
-  //     let files = { base64: [], fileList: ef };
-
-  //     for (var i = 0, len = ef.length; i < len; i++) {
-  //       let reader = new FileReader();
-  //       let f = ef[i];
-
-  //       reader.onloadend = e => {
-  //         files.base64.push(reader.result);
-
-  //         if (files.base64.length === ef.length) {
-  //           this.props.handleFiles(files);
-  //         }
-  //       }
-
-  //       reader.readAsDataURL(f);
-  //     }
-  //   } else {
-  //     let files = { base64: '', fileList: ef };
-  //     let f = ef[0];
-  //     let reader = new FileReader();
-
-  //     reader.onloadend = e => {
-  //       files.base64 = reader.result;
-  //       this.props.handleFiles(files);
-  //     }
-
-  //     reader.readAsDataURL(f);
-  //   }
-  // }
+  
 
   render() {
-    console.log(this.state.filesSrcs);
     return (
-      <div className="wrapper">
+      <Panel>
+      <Grid >
+        <Row style={{float: 'right'}}>
+          <Col xs={12} md={12}>
         <Dropzone
           accept="image/png, image/jpeg" //only accepts png and jpeg images. Uses MIME not file name extention
-          onDrop={this.handleOnDrop.bind(this)}
-          multiple={this.multipleFiles} //allow one or multiple images per product
+          onDrop={this.props.onDrop}
+          multiple={this.props.multipleFiles} //allow one or multiple images per product
           maxSize={1024 * 1024 * 5} //5MB
         >
-          
           {({ isDragActive, isDragReject, acceptedFiles, rejectedFiles }) => {
             if (isDragActive) {
-              return <div style={{direction: 'rtl', paddingRight: "20px", paddingTop: "20px", paddingLeft: "20px"}}>هذا الملف مسموح به</div>;
+              return (
+                <div
+                  style={{
+                    direction: "rtl",
+                    paddingRight: "20px",
+                    paddingTop: "20px",
+                    paddingLeft: "20px"
+                  }}
+                >
+                  هذا الملف مسموح به
+                </div>
+              );
             }
             if (isDragReject) {
-              return <div style={{direction: 'rtl', paddingRight: "20px", paddingTop: "20px", paddingLeft: "20px"}}>هذا الملف غير مسموح به</div>;
+              return (
+                <div
+                  style={{
+                    direction: "rtl",
+                    paddingRight: "20px",
+                    paddingTop: "20px",
+                    paddingLeft: "20px"
+                  }}
+                >
+                  هذا الملف غير مسموح به
+                </div>
+              );
             }
-            return acceptedFiles.length || rejectedFiles.length
-              ? <div style={{direction: 'rtl', paddingRight: "20px", paddingTop: "20px", paddingLeft: "20px"}}>`الملفات التي تم قبولها {acceptedFiles.length}, الملفات التي تم رفضها {rejectedFiles.length} `</div>
-              : <div style={{direction: 'rtl', paddingRight: "20px", paddingTop: "20px", paddingLeft: "20px"}}>يمكنك اضافة صور بسحب ملف الصورة وإلقائه هنا أو بالضغط على هذا المكان لتحميل الملف. سيتم فقط قبول ملفات الصور من نوع jpeg و png وبحجم لا يزيد عن ٥ ميجابايت</div>;
+            return acceptedFiles.length || rejectedFiles.length ? (
+              <div
+                style={{
+                  direction: "rtl",
+                  paddingRight: "20px",
+                  paddingTop: "20px",
+                  paddingLeft: "20px"
+                }}
+              >
+                `الملفات التي تم قبولها {acceptedFiles.length}, الملفات التي تم
+                رفضها {rejectedFiles.length} `
+              </div>
+            ) : (
+              <div
+                style={{
+                  direction: "rtl",
+                  paddingRight: "20px",
+                  paddingTop: "20px",
+                  paddingLeft: "20px"
+                }}
+              >
+                يمكنك اضافة صور بسحب ملف الصورة وإلقائه هنا أو بالضغط على هذا
+                المكان لتحميل الملف. سيتم فقط قبول ملفات الصور من نوع jpeg و png
+                وبحجم لا يزيد عن ٥ ميجابايت
+              </div>
+            );
           }}
         </Dropzone>
-        <aside>
+        </Col>
+        
+        <Col xs={12} md={12}>
+        
           <h2>الصور المرفوعة</h2>
-          <ul>
-            {this.state.files.map(file => (
-              <li key={file.name}>
-                <img src={file.preview} style={{ maxWidth: "72px" }} />
-              </li>
+          </Col> 
+        
+          
+            {this.props.files.map(file => (
+              <Col xs={12} md={4} key={file.name}>
+                {/* <img src={file.preview} style={{ maxWidth: "72px" }} /> */}
+                <Image src={file.preview} rounded style={{ maxWidth: "200px" }}/>
+              </Col>
             ))}
-          </ul>
-        </aside>
-        <button onClick={this.uploadImages.bind(this)}>Upload Images</button>
-      </div>
+          
+          </Row>
+        </Grid>
+      </Panel>
     );
   }
 }
--- src/components/ProductAdder.js
+++ src/components/ProductAdder.js
@@ -1,53 +1,112 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
+import { app } from "../base";
 import ImageUploader from './ImageUploader'
  
 class ProductAdder extends Component {
-  state = {
-    username: '',
-    avatar: '',
-    isUploading: false,
-    progress: 0,
-    avatarURL: ''
-  };
- 
-  handleChangeUsername = (event) => this.setState({username: event.target.value});
-  handleUploadStart = () => this.setState({isUploading: true, progress: 0});
-  handleProgress = (progress) => this.setState({progress});
-  handleUploadError = (error) => {
-    this.setState({isUploading: false});
-    console.error(error);
+  constructor(props) {
+    super(props)
+    this.state = {
+      files: []
+    };
+    //change to true if you want to upload multiple images per product
+    this.multipleFiles = false;
   }
-  handleUploadSuccess = (filename) => {
-    this.setState({avatar: filename, progress: 100, isUploading: false});
-    // firebase.storage().ref('images').child(filename).getDownloadURL().then(url => this.setState({avatarURL: url}));
-  };
  
+  
+ 
+  handleOnDrop(files, rejectedFiles) {
+    var newFiles = [];
+    //always (1) copy state value, (2) change the value, (3) then assign it back to state
+    this.multipleFiles
+      ? (newFiles = [...this.state.files, ...files])
+      : (newFiles = [...files]); //allow one image only and overwrite previous one
+    this.setState({
+      files: newFiles
+    });
+  }
+
+
+    componentWillUnmount() {
+      //to avoid memory leaks. See Important note @ (https://react-dropzone.js.org/)
+      this.state.files.map(file => {
+        window.URL.revokeObjectURL(file.preview);
+      });
+    }
+
+
+    handleUploadImages() {
+      //Check (https://firebase.google.com/docs/storage/web/upload-files) for more info
+      this.state.files.map(file => {
+        //get a reference for the image bucket (the placeholder where we will put the image into)
+        var imagesRef = app
+          .storage()
+          .ref()
+          .child("testProductImages/" + Date.now() + Math.random());
+        //upload the image. This is a task that will run async. Notice that it accepts a file as in
+        //browser API File (see https://developer.mozilla.org/en-US/docs/Web/API/File)
+        var metadata = {
+          contentType: file.type
+        };
+        //The following will return a task that will execte async
+        var uploadTask = imagesRef.put(file, metadata);
+        // Register three observers:
+        // 1. 'state_changed' observer, called any time the state changes
+        // 2. Error observer, called on failure
+        // 3. Completion observer, called on successful completion
+        uploadTask.on(
+          "state_changed",
+          function(snapshot) {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case firebase.storage.TaskState.PAUSED: // or 'paused'
+                console.log("Upload is paused");
+                break;
+              case firebase.storage.TaskState.RUNNING: // or 'running'
+                console.log("Upload is running");
+                break;
+            }
+          },
+          function(error) {
+            // Handle unsuccessful uploads
+            console.log("error uploading image of product");
+            // A full list of error codes is available at
+            // https://firebase.google.com/docs/storage/web/handle-errors
+            switch (error.code) {
+              case "storage/unauthorized":
+                // User doesn't have permission to access the object
+                break;
+  
+              case "storage/canceled":
+                // User canceled the upload
+                break;
+  
+              case "storage/unknown":
+                // Unknown error occurred, inspect error.serverResponse
+                break;
+            }
+          },
+          function() {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            var downloadURL = uploadTask.snapshot.downloadURL;
+            console.log("upload sucessful and image URL is: " + downloadURL);
+          }
+        );
+      });
+    }
+  
+
+
   render() {
     return (
       <div>
-        {/* <form>
-          <label>Username:</label>
-          <input type="text" value={this.state.username} name="username" onChange={this.handleChangeUsername} />
-          <label>Avatar:</label>
-          {this.state.isUploading &&
-            <p>Progress: {this.state.progress}</p>
-          }
-          {this.state.avatarURL &&
-            <img src={this.state.avatarURL} />
-          }
-          <FileUploader
-            accept="image/*"
-            name="avatar"
-            randomizeFilename
-            storageRef={firebase.storage().ref('images')}
-            onUploadStart={this.handleUploadStart}
-            onUploadError={this.handleUploadError}
-            onUploadSuccess={this.handleUploadSuccess}
-            onProgress={this.handleProgress}
-          />
-        </form> */}
-        <ImageUploader />
+        <ImageUploader onDrop={this.handleOnDrop.bind(this)} multipleFiles={this.multipleFiles} files={this.state.files}/>
+        <button onClick={this.handleUploadImages.bind(this)}>Upload Images</button>
+
       </div>
     );
   }
