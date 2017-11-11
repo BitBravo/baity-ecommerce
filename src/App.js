cb0a609db777fad10a6a321f81992b452599f7e8
Sumayahklf <Sumayahklf@gmail.com>
2017-11-12 01:26:01 +0300

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div style={{ margin: "0 auto" }}>
+        <div >
           <Header />
           <Main />
           <Footer />
