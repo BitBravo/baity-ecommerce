8486d98a1701efcaabf219cd3aa5dbeefcecc9cb
Muath Alkhalaf <muath.alkhalaf@gmail.com>
2018-04-14 18:45:14 +0300

    changed included file Footer.js from Selected-homepage.png to selected-homepage.png

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { IndexLinkContainer } from 'react-router-bootstrap';
 import { Glyphicon ,Modal} from "react-bootstrap";
-import Homepage from '../assets/img/Selected-homepage.png';
+import Homepage from '../assets/img/selected-homepage.png';
 import Idea from '../assets/img/Selected-idea.png';
 import Product from '../assets/img/Selected-product.png';
 import Profile from '../assets/img/Profile-icon.png';
